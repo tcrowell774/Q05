c091fdc69c587e7274a8d4068873178a4dc72d32
crowellt1 <Crowellt1@wit.edu>
2023-10-02 23:54:04 -0400

    First push

---
+++ Q5/src/module-info.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+/**
+ * @author crowellt1
+ *
+ */
+public class Q5 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.printf("What is your favorite day of the week");
+	}
+}
